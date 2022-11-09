fdee54263cbed6e1750cc99e4160831e04b40fae
Iman <74386210+iman370@users.noreply.github.com>
2022-11-09 16:50:07 +0000

    Fixed typo

--- ghufran/src/components/Proof.js
+++ ghufran/src/components/Proof.js
@@ -10,7 +10,7 @@ function Proof() {
                 <h2>Active recall is using student materials in a way that <b>actively stimulates a child's brain</b>, and helps them put their newly learnt topics into long-term memory.</h2>
             </div>
             <div id="content2">
-                <h2>Peer-reviewed studies only confirm what medical and engieering students have been using to graduate from their universities with First-Class degrees.</h2>
+                <h2>Peer-reviewed studies only confirm what medical and engineering students have been using to graduate from their universities with First-Class degrees.</h2>
                 <img className="image" src={graph1} alt="Active Recall Graph"/>
                 <h2>Using proven techniques, I can coach your child to remember what is taught, by using Active Recall and repeating this multiple times over set periods, they'll remember more than their friends.</h2>
             </div>
