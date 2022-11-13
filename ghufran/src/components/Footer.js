91b73a05acb8827658e995967516e48ac904d68b
iman370 <imaniqbal@live.co.uk>
2022-11-13 14:49:22 +0000

    Fixed footer so everything is centered properly

--- ghufran/src/components/Footer.js
+++ ghufran/src/components/Footer.js
@@ -6,8 +6,11 @@ function Footer() {
     let navigate = useNavigate()
     return(
         <div id="footer-container">
-            <h1>Ghufran Ali<br/>Studying Dentistry at Sofia MU<br/>A's in GCSE and A-Level<br/>Techniques used proved by Science</h1>
-            <Button className='button' onClick={() => navigate('../contact')}>Contact Me</Button>
+            <div id="content1"></div>
+            <div id="content2">
+                <h1>Ghufran Ali<br/>Studying Dentistry at Sofia MU<br/>A's in GCSE and A-Level<br/>Techniques used proved by Science</h1>
+                <Button className='button' onClick={() => navigate('../contact')}>Contact Me</Button>
+            </div>
         </div>
     )
 }
