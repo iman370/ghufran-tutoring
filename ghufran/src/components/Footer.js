3b135165b4d0735c07b1718c008ea3b78f416b96
Iman <74386210+iman370@users.noreply.github.com>
2025-09-14 20:23:00 +0100

    Update Footer.js
    Changed from 3rd year to 5th

--- ghufran/src/components/Footer.js
+++ ghufran/src/components/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
         <div id="footer-container">
             <div id="content1"></div>
             <div id="content2">
-                <h1>Ghufran Ali<br/>Third Year Dental Student<br/>A's in GCSE and A-Level<br/>Techniques used proved by Science</h1>
+                <h1>Ghufran Ali<br/>Fifth Year Dental Student<br/>A's in GCSE and A-Level<br/>Techniques used proved by Science</h1>
                 <Button className='button' onClick={() => navigate('../contact')}>Contact Me</Button>
             </div>
         </div>
